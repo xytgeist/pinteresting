5d84367877c0c223a33bf3253eb19e0bbcbd059f
Ryan Franklin <investigence@gmail.com>
2013-10-20 19:47:43 -0400

    created My Courses page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Pinteresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"   #creates about_path
-  
+  get "mycourses" => "pages#mycourses"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
